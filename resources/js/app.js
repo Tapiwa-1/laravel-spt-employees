e55bcd419cc98c23d242a928f0d9ff06cd5a3651
tapiwa-1 <tmotsi10@gmail.com>
2023-06-17 09:14:53 +0200

    add register

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,12 @@ import "@protonemedia/laravel-splade/dist/style.css";
 import { createApp } from "vue/dist/vue.esm-bundler.js";
 import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
 
+
 const el = document.getElementById("app");
 
 createApp({
-    render: renderSpladeApp({ el })
-})
+        render: renderSpladeApp({ el })
+    })
     .use(SpladePlugin, {
         "max_keep_alive": 10,
         "transform_anchors": false,
